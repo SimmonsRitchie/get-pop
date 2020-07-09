2c67c9bc6fd495c0e256c6ed5886606953b672fe
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-09 11:21:23 -0400

    tidy up

--- setup.py
+++ setup.py
@@ -5,13 +5,20 @@ with open("README.md", "r") as fh:
 
 setup(
     name="get_pop",
-    version="1.1",
+    version="1.4",
     author="DSR",
     description="Command line tool to generates CSVs of population data for specified US states.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     # list folders, not files
-    packages=["get_pop"],
-    scripts=["get_pop/bin/run_script.py"],
+    package_dir={"get_pop": "get_pop"},
+    packages=[
+        "get_pop",
+        "get_pop/modules",
+        "get_pop/modules/helper",
+        "get_pop/modules/init",
+        "get_pop/modules/parse",
+    ],
+    scripts=["get_pop/bin/getpop"],
     package_data={"get_pop": ["static/usa_pop_counties_2019.csv"]},
 )
