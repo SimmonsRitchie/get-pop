e2124d5b31a44db9a38caaf96b66dbda77810278
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-10 12:23:17 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="get_pop",
-    version="1.91",
+    version="1.92",
     author="DSR",
     description="Command line tool to generates CSVs of population data for specified US states.",
     long_description=long_description,
