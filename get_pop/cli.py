df9c75b156aaa03b7729f3dead33a1e56d38ae77
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-10 12:23:01 -0400

    Tidy up, refactor CLI into separate module, fix logging import issue

---
+++ get_pop/cli.py
@@ -0,0 +1,11 @@
+import click
+from get_pop.get_pop import get_pop
+from get_pop.static.constants import state_index
+
+clean_states = [x["abbrv"] for x in state_index]
+
+
+@click.command()
+@click.argument("states", nargs=-1, type=click.Choice(clean_states))
+def main(states: tuple) -> None:
+    get_pop(states)
