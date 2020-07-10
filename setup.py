cef9db7f3589ee2c3bfa02a19912cc239797403e
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-10 17:53:54 -0400

    minor changes

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ with open("README.md", "r") as fh:
 
 setup(
     name="get_pop",
-    version="1.93",
+    version="1.94",
     author="DSR",
-    description="Command line tool that generates CSVs of county-level population data for specified US states.",
+    description="A python command line utility that generates CSVs of county-level population data for specified US "
+    "states.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python",
