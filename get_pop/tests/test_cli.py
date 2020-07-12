355c59ac8eb18b028ffb84b14d764d6b582abf71
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-12 17:39:20 -0400

    Add new test

--- get_pop/tests/test_cli.py
+++ get_pop/tests/test_cli.py
@@ -11,3 +11,15 @@ def test_cli_creates_file(tmpdir):
     # assert
     assert (tmpdir / "tx-county-pop.csv").is_file()
     assert not (tmpdir / "ny-county-pop.csv").is_file()
+
+
+def test_cli_creates_files(states, tmpdir):
+    # test
+    tmpdir = Path(tmpdir)
+    subprocess.run(["getpop", *states, "--dir", tmpdir])
+
+    # assert
+    expected_paths = [tmpdir / f"{x}-county-pop.csv" for x in states]
+    for path in expected_paths:
+        assert path.is_file()
+    assert not (tmpdir / "ca-county-pop.csv").is_file()
