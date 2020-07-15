e54e966e1d2841eea09ba297ae421bb37b1530e3
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-14 22:35:12 -0400

    Fix tests to use new CLI API

--- get_pop/tests/test_cli.py
+++ get_pop/tests/test_cli.py
@@ -6,7 +6,7 @@ from pathlib import Path
 def test_cli_creates_file(tmpdir):
     # test
     tmpdir = Path(tmpdir)
-    subprocess.run(["getpop", "tx", "--dir", tmpdir])
+    subprocess.run(["getpop", "tx", "-d", tmpdir])
 
     # assert
     assert (tmpdir / "tx-county-pop.csv").is_file()
@@ -16,7 +16,7 @@ def test_cli_creates_file(tmpdir):
 def test_cli_creates_files(states, tmpdir):
     # test
     tmpdir = Path(tmpdir)
-    subprocess.run(["getpop", *states, "--dir", tmpdir])
+    subprocess.run(["getpop", *states, "-d", tmpdir])
 
     # assert
     expected_paths = [tmpdir / f"{x}-county-pop.csv" for x in states]
