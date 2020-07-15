f1a6976fc6e7f3acbf46bab06fa7cd1ee0aa032e
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-14 22:26:00 -0400

    Add quiet mode

--- get_pop/cli.py
+++ get_pop/cli.py
@@ -4,6 +4,8 @@ from typing import Union
 from get_pop.definitions import CWD
 from get_pop.get_pop import get_pop
 from get_pop.static.constants import state_index
+import os
+import sys
 
 clean_states = [x["abbrv"] for x in state_index] + ["all"]
 
@@ -12,12 +14,19 @@ clean_states = [x["abbrv"] for x in state_index] + ["all"]
 @click.argument("states", nargs=-1, type=click.Choice(clean_states))
 @click.option(
     "--save-dir",
-    "--dir",
+    "-d",
     default=CWD,
     help="Path of directory where CSV files will be output. Defaults to saving in the current "
     "working directory",
 )
-def main(states: tuple, save_dir: Union[str, Path]) -> None:
+@click.option(
+    "--quiet",
+    "-q",
+    is_flag=True,
+    default=False,
+    help="Disables stdout during program run",
+)
+def main(states: tuple, save_dir: Union[str, Path], quiet: bool) -> None:
     """
     CLI API using click library. Generates CSVs of county-level state data based on
     provided arguments.
@@ -31,5 +40,9 @@ def main(states: tuple, save_dir: Union[str, Path]) -> None:
     Returns:
         None
     """
+    if quiet:
+        f = open(os.devnull, "w")
+        sys.stdout = f
+
     states_list = list(states)  # convert from tuple to list
     get_pop(states_list, save_dir=save_dir)
